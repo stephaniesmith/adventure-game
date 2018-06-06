c36f1eae597de0c64ff7cd8bf8449c2461c98c5f
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-06 09:56:18 -0700

    added items to the players inventory

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ChooseItem from './ChooseItem';
 
 export default class Player extends Component {
   static propTypes = {
-    player: PropTypes.object.isRequired
+    player: PropTypes.object.isRequired,
+    onUse: PropTypes.func.isRequired
   };
 
   render() {
-    const { player } = this.props;
+    const { player, onUse } = this.props;
     const { name, inventory } = player;
 
     return (
       <div>
         <h2>{name}</h2>
-        <p>{inventory[0]}</p>
+        <ul>
+          <ChooseItem items={inventory} onChoose={onUse}/>
+        </ul>
       </div>
     );
   }
